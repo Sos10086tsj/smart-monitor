210a19753f2d2c97677d40ee68e02757a80af18e
Paris Tao <paristao1989@gmail.com>
2017-02-07 10:25:13 +0800

    调整access time info展示结构

--- src/main/java/com/chinesedreamer/smartmonitor/activemq/task/impl/MqServerMonitorTaskImpl.java
+++ src/main/java/com/chinesedreamer/smartmonitor/activemq/task/impl/MqServerMonitorTaskImpl.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.chinesedreamer.smartmonitor.activemq.service.ActiveMqService;
 import com.chinesedreamer.smartmonitor.activemq.task.MqServerMonitorTask;
 import com.chinesedreamer.smartmonitor.activemq.vo.MqServerMonitorVelocityVo;
@@ -83,7 +86,7 @@ public class MqServerMonitorTaskImpl implements MqServerMonitorTask{
 							vo.setWarning(0);
 							StringBuilder builder = new StringBuilder();
 							for (int i = 0; i < datas.size(); i++) {
-								builder.append(datas.get(i).toString());
+								this.appendTimeInfo(builder, datas.get(i).toString());
 								if (i < datas.size() - 1) {
 									builder.append("</br>");
 								}
@@ -108,4 +111,21 @@ public class MqServerMonitorTaskImpl implements MqServerMonitorTask{
 		this.threadPoolTaskExecutor.execute(MailSender.getInstance(mailSenderService, "ActiveMQ Server Status Tunning", model, pu.getProperty("monitor.activemq.mail.to").split(","), "velocity/api_borker_status_monitor.vm"));
 		this.logger.info(" End to monitor MQ Serve status task.");
 	}
+	
+	private void appendTimeInfo(StringBuilder builder, String info) {
+		if (StringUtils.isEmpty(info)) {
+			return;
+		}
+		if (info.startsWith("{")) {
+			info = info.substring(1, info.length());
+		}
+		if (info.endsWith("}")) {
+			info = info.substring(0, info.length() - 1);
+		}
+		String[] mapStr = info.split("=");
+		if (mapStr.length > 2) {
+			builder.append(mapStr[1]);
+		}
+	}
+	
 }
