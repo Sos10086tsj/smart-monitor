dde00fae7dca50a45fc3eeaf6a74a081809b403a
Paris Tao <paristao1989@gmail.com>
2017-02-07 10:26:02 +0800

    调整access time info展示结构

--- src/main/java/com/chinesedreamer/smartmonitor/activemq/task/impl/MqServerMonitorTaskImpl.java
+++ src/main/java/com/chinesedreamer/smartmonitor/activemq/task/impl/MqServerMonitorTaskImpl.java
@@ -16,8 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.chinesedreamer.smartmonitor.activemq.service.ActiveMqService;
 import com.chinesedreamer.smartmonitor.activemq.task.MqServerMonitorTask;
 import com.chinesedreamer.smartmonitor.activemq.vo.MqServerMonitorVelocityVo;
